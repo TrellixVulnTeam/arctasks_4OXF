804b99c8daf5e43381d51f41a764be37eefb4a5d
Wyatt Baldwin <wbaldwin@pdx.edu>
2015-06-24 16:28:56 -0700

    Add django tasks makemigrations, migrate, and test
    Because the django test task conflicts with the test configure task and
    because the test configure task is kind of a special case (in that it's
    rare that it needs to be specified on the command line), removed the
    latter from the exported API in favor of the former.

--- arctasks/django.py
+++ arctasks/django.py
@@ -1,7 +1,8 @@
 from invoke.tasks import ctask as task
 
-from .config import configured
+from .config import configure, configured
 from .runners import local
+from .util import abort
 
 
 @task(configured)
@@ -9,3 +10,22 @@ def manage(ctx, args, cd=None, sudo=False, run_as=None, echo=True, hide=False,
            abort_on_failure=True):
     """Run a Django management command."""
     local(ctx, ('{python}', 'manage.py', args), cd, sudo, run_as, echo, hide, abort_on_failure)
+
+
+@task(configured)
+def makemigrations(ctx, app=''):
+    manage(ctx, ('makemigrations', app))
+
+
+@task(configured)
+def migrate(ctx, app='', migration=''):
+    args = ('migrate', app, migration)
+    if migration and not app:
+        abort(1, 'You must specify an app to run a specific migration')
+    manage(ctx, args)
+
+
+@task
+def test(ctx, test=''):
+    configure(ctx, 'test')
+    manage(ctx, ('test', test))
