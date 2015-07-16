206a938b46c66ec2931d151205d874e009c3939d
Wyatt Baldwin <wbaldwin@pdx.edu>
2015-07-15 17:23:10 -0700

    Add --with-postgis flag to create_postgresql_db()
    This flag can be passed to createdb(), which will pass it through.

--- arctasks/db.py
+++ arctasks/db.py
@@ -3,20 +3,25 @@ from .runners import local
 
 
 @arctask(configured='dev')
-def createdb(ctx, type=None, name='{db.name}', drop=False):
+def createdb(ctx, type=None, name='{db.name}', drop=False, with_postgis=False):
     if type is None:
         type = ctx.db.type
     if type == 'mysql':
         create_mysql_db(ctx, name, drop)
     elif type == 'postgresql':
-        create_postgresql_db(ctx, name, drop)
+        create_postgresql_db(ctx, name, drop, with_postgis)
     else:
         raise ValueError('Unknown database type: {db.type}'.format(**ctx))
 
 
 @arctask
-def create_postgresql_db(ctx, name='{db.name}', drop=False):
-    """Create a PostgreSQL database with the specified ``name``."""
+def create_postgresql_db(ctx, name='{db.name}', drop=False, with_postgis=False):
+    """Create a PostgreSQL database with the specified ``name``.
+
+    The --with-postgis flag can be used to spatially-enable the
+    database. This only works with PostgreSQL 9.1+ and PostGIS 2.0+.
+
+    """
     # Try to run the drop and create commands with the postgres user; if
     # that user doesn't exist, run those commands as the current user.
     # This supports VM and Homebrew setups.
@@ -28,6 +33,8 @@ def create_postgresql_db(ctx, name='{db.name}', drop=False):
     if drop:
         local(ctx, ('dropdb', name), **args)
     local(ctx, ('createdb', name), **args)
+    if with_postgis:
+        local(ctx, ('psql -d', name, '-c "CREATE EXTENSION postgis;"'))
 
 
 @arctask
