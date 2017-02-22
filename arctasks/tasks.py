f482857ad2b4033aab91c13b7b73ae1e67ede165
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-02-22 14:40:17 -0800

    Add explicit db.host for dev and test (remove default)

---
+++ arctasks/tasks.py
@@ -0,0 +1,10 @@
+from taskrunner.tasks import *
+
+from .base import clean, install, lint, npm_install, retrieve, virtualenv
+from .db import createdb, load_prod_data, reset_db
+from .deploy import builds, clean_builds, deploy, link, restart, push_static
+from .django import coverage, dbshell, makemigrations, migrate, runserver, run_mod_wsgi, shell, test
+from .python import show_upgraded_packages
+from .release import release, prepare_release, merge_release, tag_release, resume_development
+from .static import build_css, build_static, bower, collectstatic, lessc, pull_media, sass
+from .timetracking import time_spent
