acbb0b7256d115444d0fedec7dc3931159ebe0b9
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-03-13 09:26:30 -0700

    Add sudo option to rsync task

--- arctasks/remote.py
+++ arctasks/remote.py
@@ -22,10 +22,9 @@ _rsync_default_excludes = ('__pycache__/', '.DS_Store', '*.pyc', '*.swp')
 
 
 @task
-def rsync(config, local_path, remote_path, user='{remote.user}', host='{remote.host}',
-          run_as='{remote.run_as}', dry_run=False, delete=False, excludes=(),
-          default_excludes=_rsync_default_excludes, echo=True, hide='all', mode=_rsync_default_mode,
-          source='local'):
+def rsync(config, local_path, remote_path, user=None, host=None, sudo=False, run_as=None,
+          dry_run=False, delete=False, excludes=(), default_excludes=_rsync_default_excludes,
+          echo=True, hide=None, mode=_rsync_default_mode, source='local'):
     """Copy files using rsync.
 
     By default, this pushes from ``local_path`` to ``remote_path``. To
@@ -34,22 +33,32 @@ def rsync(config, local_path, remote_path, user='{remote.user}', host='{remote.h
 
     """
     remote_path = '{user}@{host}:{remote_path}'.format(**locals())
+
     if source == 'local':
         source_path, destination_path = local_path, remote_path
     elif source == 'remote':
         source_path, destination_path = remote_path, local_path
     else:
         raise ValueError('source must be either "local" or "remote"')
+
     excludes = as_tuple(excludes)
     if default_excludes:
         excludes += as_tuple(default_excludes)
-    run_as = args_to_str(run_as, format_kwargs=config)
+
+    if sudo:
+        rsync_path = '--rsync-path "sudo rsync"'
+    elif run_as and run_as != user:
+        run_as = args_to_str(run_as, format_kwargs=config)
+        rsync_path = '--rsync-path "sudo -u {run_as} rsync"'.format_map(locals())
+    else:
+        rsync_path = None
+
     local(config, (
         'rsync',
         '-rltvz',
         '--dry-run' if dry_run else '',
         '--delete' if delete else '',
-        '--rsync-path "sudo -u {run_as} rsync"'.format(run_as=run_as) if run_as else '',
+        rsync_path,
         '--no-perms', '--no-group', '--chmod=%s' % mode,
         tuple("--exclude '{p}'".format(p=p) for p in excludes),
         source_path, destination_path,
