63258d040e92c811629b89ed4980b7e7867c69d4
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-04-26 11:35:46 -0700

    Upgrade setuptools 34.4.1 => 35.0.1

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 
 install_requires = [
     'boto3>=1.4.4',
-    'setuptools>=34.4.1',
     'runcommands>=1.0a17',
+    'setuptools>=35.0.1',
 ]
 
 if sys.version_info[:2] < (3, 4):
