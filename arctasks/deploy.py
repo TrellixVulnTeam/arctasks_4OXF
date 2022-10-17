ce5fbae10ef1f4d6b3c8251227c80e059f2a3cc3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 01:51:16 +0000

    Adding tarfile member sanitization to extractall()

--- arctasks/deploy.py
+++ arctasks/deploy.py
@@ -223,7 +223,26 @@ class Deployer:
             tarball_path = os.path.join(build_dir, 'virtualenv.tgz')
             urlretrieve(config.virtualenv.download_url, tarball_path)
             with tarfile.open(tarball_path, 'r') as tarball:
-                tarball.extractall(build_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tarball, build_dir)
             os.remove(tarball_path)
             os.rename(
                 os.path.join(build_dir, config.virtualenv.base_name),
